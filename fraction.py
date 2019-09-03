ce76b303c33dcfa0fb68548612d4af7958d94bd1
StanislauSupronenka <54594393+StanislauSupronenka@users.noreply.github.com>
2019-09-03 21:05:35 +0300

    v 1.1

--- fraction.py
+++ fraction.py
@@ -1,5 +1,13 @@
 #! usr/lib/python3
 # coding:utf-8
+import math
+
+
+def simplify(numerator, denominator):
+    smallest = min(numerator, denominator)
+    for i in range(smallest, 1, -1):
+        if numerator % i == 0 and denominator % i == 0:
+            return int(numerator / i), int(denominator / i)
 
 
 class Fraction:
@@ -8,38 +16,22 @@ class Fraction:
         self.numerator = numerator
         self.denominator = denominator
 
-    def simplify(self, numerator, denominator):
-        self.numerator = numerator
-        self.denominator = denominator
-        if numerator != denominator:
-            divider = 2
-            while divider <= self.denominator:
-                if self.numerator % divider == 0:
-                    if self.denominator % divider == 0:
-                        self.numerator /= divider
-                        self.denominator /= divider
-                        divider = 2
-                divider += 1
-            return str(int(self.numerator)) + '/' + str(int(self.denominator))
-        else:
-            return numerator
-
     def __add__(self, fraction):
-        self.numerator = fraction.denominator*self.numerator + fraction.numerator * self.denominator
+        self.numerator = fraction.denominator * self.numerator + fraction.numerator * self.denominator
         self.denominator = self.denominator * fraction.denominator
-        return self.simplify(self.numerator, self.denominator)
+        return simplify(self.numerator, self.denominator)
 
     def __sub__(self, fraction):
         self.numerator = self.numerator * fraction.denominator - self.denominator * fraction.numerator
         self.denominator = self.denominator * fraction.denominator
-        return self.simplify(self.numerator, self.denominator)
+        return simplify(self.numerator, self.denominator)
 
     def __mul__(self, fraction):
         self.numerator = self.numerator * fraction.numerator
         self.denominator = self.denominator * fraction.denominator
-        return self.simplify(self.numerator, self.denominator)
+        return simplify(self.numerator, self.denominator)
 
     def __truediv__(self, fraction):
         self.numerator = self.numerator * fraction.denominator
         self.denominator = self.denominator * fraction.numerator
-        return self.simplify(self.numerator, self.denominator)
+        return simplify(self.numerator, self.denominator)
